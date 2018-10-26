6d62715296880dd051c1f9387faaef4e614332e9
Le Ba Cong <congmb@gmail.com>
2018-10-26 15:42:55 +0700

    first commit

---
+++ demo/uet/oop/bomberman/graphics/SpriteTest.java
@@ -0,0 +1,25 @@
+package uet.oop.bomberman.graphics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpriteTest {
+
+    @Test
+    void movingSprite() {
+    }
+
+    @Test
+    void movingSprite1() {
+    }
+
+    @Test
+    void getSize() {
+        System.out.println(Sprite.grass.getSize());
+    }
+
+    @Test
+    void getPixel() {
+    }
+}
---
+++ src/uet/oop/bomberman/level/LevelLoader.java
@@ -0,0 +1,47 @@
+package uet.oop.bomberman.level;
+
+import uet.oop.bomberman.Board;
+import uet.oop.bomberman.exceptions.LoadLevelException;
+
+/**
+ * Load và lưu trữ thông tin bản đồ các màn chơi
+ */
+public abstract class LevelLoader {
+
+	protected int _width = 20, _height = 20; // default values just for testing
+	protected int _level;
+	protected Board _board;
+
+	public LevelLoader(Board board, int level) throws LoadLevelException {
+		_board = board;
+		loadLevel(level);
+	}
+
+	public abstract void loadLevel(int level) throws LoadLevelException;
+
+	public abstract void createEntities();
+
+	public int getWidth() {
+		return _width;
+	}
+
+	public int getHeight() {
+		return _height;
+	}
+
+	public int getLevel() {
+		return _level;
+	}
+
+	public void set_width(int _width) {
+		this._width = _width;
+	}
+
+	public void set_height(int _height) {
+		this._height = _height;
+	}
+
+	public void set_level(int _level) {
+		this._level = _level;
+	}
+}
