8ca21fc09f987c7aa25ad5c6142a5e7c9934dab2
nam3n <37288434+nam3n@users.noreply.github.com>
2018-11-07 00:57:18 +0700

    Tạo các đoạn Flame.

--- BoombermanClient/src/uet/oop/bomberman/entities/bomb/Flame.java
+++ BoombermanClient/src/uet/oop/bomberman/entities/bomb/Flame.java
@@ -2,6 +2,7 @@ package uet.oop.bomberman.entities.bomb;
 
 import uet.oop.bomberman.Board;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.character.Character;
 import uet.oop.bomberman.graphics.Screen;
 
 public class Flame extends Entity {
@@ -45,6 +46,20 @@ public class Flame extends Entity {
 		boolean last;
 
 		// TODO: tạo các segment dưới đây
+
+        int x = (int)_x;
+        int y = (int)_y;
+        for (int i = 0; i < _flameSegments.length; i++) {
+            last = (i == _flameSegments.length - 1);
+
+            switch (_direction) {
+                case 0: y--; break;
+                case 1: x++; break;
+                case 2: y++; break;
+                case 3: x--; break;
+            }
+            _flameSegments[i] = new FlameSegment(x, y, _direction, last);
+        }
 	}
 
 	/**
@@ -53,7 +68,26 @@ public class Flame extends Entity {
 	 */
 	private int calculatePermitedDistance() {
 		// TODO: thực hiện tính toán độ dài của Flame
-		return 1;
+		int radius = 0;
+		int x = (int)_x;
+		int y = (int)_y;
+
+		while(radius < _radius) {
+			if(_direction == 0) y--;
+			if(_direction == 1) x++;
+			if(_direction == 2) y++;
+			if(_direction == 3) x--;
+
+			Entity a = _board.getEntity(x, y, null);
+
+			if(a instanceof Character) ++radius;
+
+			if(!a.collide(this))
+				break;
+
+			++radius;
+		}
+		return radius;
 	}
 	
 	public FlameSegment flameSegmentAt(int x, int y) {
